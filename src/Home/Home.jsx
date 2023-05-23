acba6717253cd77cb33e21b4235c7a79d73654b6
arshadrahman02 <shaikarshadrahman02@gmail.com>
2023-05-24 02:05:45 +0600

    tailwind added

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-red-300">This is Home Component</h1>
+    </div>
+  );
+};
+
+export default Home;

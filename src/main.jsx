970a52fcfc2ba96a88f96771df7c973bbfe5b3c1
arshadrahman02 <shaikarshadrahman02@gmail.com>
2023-05-24 02:21:27 +0600

    navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { router } from "./Route/Route.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="max-w-6xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );

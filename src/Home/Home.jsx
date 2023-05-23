c39bb7de780f9b79957388f049da773e706003b7
arshadrahman02 <shaikarshadrahman02@gmail.com>
2023-05-24 02:07:58 +0600

    daisy ui added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-xl text-red-300">This is Home Component</h1>
+      <h1 className="text-xl text-red-300 bg-primary">
+        This is Home Component
+      </h1>
     </div>
   );
 };

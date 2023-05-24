2fc6411b8fc66e06a488b3dab30bd20873910fc9
arshadrahman02 <shaikarshadrahman02@gmail.com>
2023-05-25 00:52:44 +0600

    call footer

--- src/Home/CallUsFooter/CallUsFooter.jsx
+++ src/Home/CallUsFooter/CallUsFooter.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const CallUsFooter = () => {
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content my-20 w-80 mx-auto">
-      <div className="container">Call US : +88 0191234567810</div>
+    <footer className="footer p-10 bg-neutral text-white my-20 lg:w-[1000px] mx-auto">
+      <div className="container lg:mx-64 text-xl lg:text-3xl">
+        Call US : +88 0191234567810
+      </div>
     </footer>
   );
 };

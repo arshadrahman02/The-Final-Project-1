d4c4b0584e05232ba921d3d40d337b0ee2a375e6
arshadrahman02 <shaikarshadrahman02@gmail.com>
2023-05-25 10:58:19 +0600

    menu pages works start

--- src/Home/Reviews/Reviews.jsx
+++ src/Home/Reviews/Reviews.jsx
@@ -4,6 +4,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import { FaHeadset } from "react-icons/fa";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
@@ -27,7 +30,18 @@ const Reviews = () => {
       >
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
-            <div className="m-24 text-center">
+            <div>
+              <Rating
+                className="mx-auto"
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <div>
+                <FaHeadset className="text-5xl mx-auto mt-10"></FaHeadset>
+              </div>
+            </div>
+            <div className=" mt-10 text-center px-20 mb-5">
               <p>{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
             </div>
